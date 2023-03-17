3fc516a0f9fd86658d296a3dfbd07e2b2d3bf3a6
Sijawusz Pur Rahnama <sija@sija.pl>
2023-03-17 03:57:54 +0100

    Fix `undefined local variable` error in `Session#find_elements` (#40)
    * Fix `undefined local variable` error
    
    * Fix call to non-existent `Session#find_elements!`

--- src/marionette/session.cr
+++ src/marionette/session.cr
@@ -447,7 +447,7 @@ module Marionette
 
     # Find multiple elements with the given selector and return them as
     # an array.
-    def find_elements(selector, strategy : LocationStrategy = :css)
+    def find_elements(selector, strategy : LocationStrategy = :css, parent : Element? = nil)
       how, what = strategy.convert_locator(selector)
 
       # TODO: Add support for this via Support::RelativeLocator
@@ -480,7 +480,7 @@ module Marionette
     # Find multiple children of the given `element` with the given selector and
     # return them as an array.
     def find_element_children(element, selector, strategy : LocationStrategy = :css)
-      find_elements!(selector, strategy, element)
+      find_elements(selector, strategy, element)
     end
 
     # Wait the given amount of time for an element to be available.
